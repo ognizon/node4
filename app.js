8a83671ae41ae45d30e06c27494e7aee800d31e0
tech4all <ognizon@gmail.com>
2024-11-05 16:21:12 +0530

    styles

--- app.js
+++ app.js
@@ -2,27 +2,33 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
-const app1 = express();
+const app = express();
 
 //middleware urlencoded
-app1.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}));
 //middleware json
-app1.use(express.json());
+app.use(express.json());
 //middleware static
-app1.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //route handling
-app1.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'pages', 'index.html'));
 });
-app1.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname,'pages', 'aboutus.html'));
 });
-//Error  Handling: custom middleware
-app1.use((req, res) => {
+// All other requests error page
+app.get('*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname,'pages', '404.html'));
 });
 
-app1.listen(3001, () => {
+
+// //Error  Handling: custom middleware
+// app1.use((req, res) => {
+//     res.status(404).sendFile(path.join(__dirname,'pages', '404.html'));
+// });
+
+app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
